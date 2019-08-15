345f5ad4d24c48edb661292bf2273521cc56073b
Daniel Packard <daniel.packard@complexrehabsystems.com>
2019-08-15 13:33:47 -0500

    re-add SerialCommands example

---
+++ examples/SerialCommands/SerialCommands.ino
@@ -0,0 +1,148 @@
+#include "StateMachine.h"
+#include "Log.h"
+
+StateMachine sm;
+StateMachine commands;
+int yellowLight = LOW;
+int greenLight = LOW;
+
+String input;
+
+int executionCount = 0;
+
+void setup() {
+
+  DisableLog();
+
+  Serial.begin(9600);
+
+  sm.AddState("Message", [] () {
+
+    //Example: Print a line to the serial monitor
+    Serial.println("Enter the information you would like printed:");
+
+  });
+
+  sm.AddState("Read", [] () {
+
+    if(Serial.available())
+    {
+      input = Serial.readString();
+      input.trim();
+    }
+
+  });
+
+  sm.AddState("Execute", [] () {
+
+    Serial.println("The information you input is: " + input);
+    auto commandState = commands.GetNextState();
+    commands.SetCurrentState(commandState);
+    Serial.println("Current state is: " + commandState);
+    commands.RunCurrentState();
+    commands.SetCurrentState("none");
+    executionCount++;
+
+  });
+
+  sm.AddTransition("Message", "Read", []() -> bool {
+
+    return true;
+
+  });
+
+  sm.AddTransition("Read", "Execute", []() -> bool {
+
+    //This function should return a boolean that is true when the 
+    //transition should be made
+    return !input.equals("");
+
+  });
+
+  sm.AddTransition("*", "Message", []() -> bool {
+
+    //This function should return a boolean that is true when the 
+    //transition should be made
+    return input.equals("repeat");
+
+  });
+
+  sm.AddTransition("Execute", "Message", []() -> bool {
+
+    bool transition = false;
+    if(executionCount>0)
+    {
+      transition = true;
+      executionCount = 0;
+    }
+    ClearInput();
+    return transition;
+    
+  });
+
+  SetUpCommands();
+
+  sm.SetCurrentState("Message");
+  sm.RunCurrentState();
+
+}
+
+void loop() {
+
+  auto nextState = sm.GetNextState();
+  sm.SetCurrentState(nextState);
+  sm.RunCurrentState();
+
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------
+
+void ClearInput()
+{
+  input = "";
+}
+
+void SetUpCommands()
+{
+
+  commands.AddState("none", [] () {
+
+  });
+
+  commands.AddState("printMessage", [] () {
+    Serial.println("This is the message for the printMessage command");
+    if(greenLight == LOW){
+      greenLight = HIGH;
+    }
+    else{
+      greenLight = LOW;
+    }
+    digitalWrite(4, greenLight);
+  });
+
+  commands.AddTransition("*", "printMessage", []() -> bool {
+
+    return input.equals("printMessage");
+    
+  });
+
+  commands.AddState("printMessage2", [] () {
+    Serial.println("This is the message for the printMessage2 command");
+    if(yellowLight == LOW){
+      yellowLight = HIGH;
+    }
+    else{
+      yellowLight = LOW;
+    }
+    digitalWrite(7, yellowLight);
+  });
+
+  commands.AddTransition("*", "printMessage2", []() -> bool {
+
+    return input.equals("printMessage2");   
+    
+  });
+
+  commands.SetCurrentState("none");
+  
+}
